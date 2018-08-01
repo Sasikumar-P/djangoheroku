01bd58309c2925c2aaed82b84d45b4d0fdfa07f3
sk <skp.guptha@gmail.com>
2018-08-01 08:37:12 +0530

    got gihub

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MyProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': []i,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
